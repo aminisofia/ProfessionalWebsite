af96c8167a318515ed435654a264402f5f1e00b8
Sofia Aminifard <aminisofia@icloud.com>
2026-02-10 18:13:54 -0800

    fixed animations

--- animations.js
+++ animations.js
@@ -1,9 +1,27 @@
-// Source - https://stackoverflow.com/a/19800010
-// Posted by Renato Zannon, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-02-10, License - CC BY-SA 3.0
-
+let spinning = false;
 function spin() {
+    if (spinning) return;
+    spinning = true;
+
     var img = document.getElementById("profile");
-    img.setAttribute("class", "rotated-image");
+    img.style.transform = "rotate(0deg)";
+    let degrees = 0;
+    const interval = setInterval(function () {
+        degrees+=1.5;
+        let easedDegrees = lerp(0, 360, easeInOutSine(degrees / 360));
+        img.style.transform = "rotate(" + easedDegrees + "deg)";
+
+        if (degrees >= 360) {
+            spinning = false;
+            clearInterval(interval);
+        }
+    }, 1);
 }
 
+function lerp(a, b, t) {
+    return (1 - t) * a + (t) * b;
+}
+
+function easeInOutSine(x) {
+    return -(Math.cos(Math.PI * x) - 1) / 2;
+}
